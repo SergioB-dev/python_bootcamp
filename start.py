6a4e373ef8bc325700dd8db1d1deed0ad9daee42
Sergio <bostshakur@gmail.com>
2022-06-16 20:09:51 -0700

    task 1 question

--- start.py
+++ start.py
@@ -1,3 +1,6 @@
+from copy import copy
+
+
 def add(a,b):
     return a + b
 
@@ -7,5 +10,25 @@ countries = ['ghana', 'saudi', 'nigeria', 'yemen', 'sudan']
 
 # Task 1: create a function that takes a list and spits out the list in alphabetical order
 
+index = 1
 
+for country in countries:
+    print(f'{index}: ', country)
+    index += 1
 
+def alphabetize(some_list):
+
+    results = copy(some_list)
+    not_in_order_results = []
+    for i in range(0, len(some_list) - 1):
+        if some_list[i][0] < some_list[i + 1][0]:
+            pass
+        elif some_list[i][0] < some_list[i + 2][0]:
+            pass
+
+        
+    print(not_in_order_results)
+
+    return results
+
+print(alphabetize(countries))
