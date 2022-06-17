c62ea4efb81505df4e6e3134f2202e1c2d73790c
Sergio <bostshakur@gmail.com>
2022-06-16 19:37:47 -0700

    create task 1

--- start.py
+++ start.py
@@ -2,4 +2,10 @@ def add(a,b):
     return a + b
 
 
-# lists [] , dictionary {x:y}, 
+countries = ['ghana', 'saudi', 'nigeria', 'yemen', 'sudan']
+
+
+# Task 1: create a function that takes a list and spits out the list in alphabetical order
+
+
+
